fa1fe5527064fb924acb1100c3901f3b73f1a2aa
Ian kim faa <48971875+ikfaa1207@users.noreply.github.com>
2024-05-21 02:23:19 +0800

    Add files via upload

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Vlad Lend</title>
+    <meta name="Author" content="<Vlad Lend>" />
+    <meta name="Publisher" content="<Vlad Lend>" />
+    <meta name="Copyright" content="<Vlad Lend>" />
+    <meta name="Description" content="Vlad Lend is a lending management platform made for kuya Vlad." />
+    <meta name="Keywords" content="kuya, lend, vlad, lending, management, platform" />
+    <link href="../css/styles.css" rel="stylesheet">
+    <!--font awesome cdn link-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
+    <!--bootstrap cdn link-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!--Jquery cdn link-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+</head>
+
+<body>
